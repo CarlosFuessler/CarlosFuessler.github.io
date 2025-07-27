02dec02edadc4fefbbfffb8db00df47b8fd9eaf9
Carlos <181351713+CarlosFuessler@users.noreply.github.com>
2025-07-27 17:03:11 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,17 @@ import { Analytics } from "@vercel/analytics/next"
 function Main_Container() {
   return (
     <div className="min-h-screen w-full bg-gray-600 flex">
-          <div className="flex flex-row">
-          <div className="flex bg-amber-400 w-20"></div>
-          <div className="flex bg-amber-500 w-20"></div>
-          <div className="flex bg-amber-600 w-20"></div>
-          <div className="flex bg-amber-700 w-20"></div>
-          <div className="flex bg-amber-800 w-20"></div>
-
+        <div className="flex flex-row w-screen h-screen">
+          <div className="flex bg-amber-400 flex-1"></div>
+          <div className="flex bg-amber-500 flex-1"></div>
+          <div className="flex bg-amber-600 flex-1"></div>
+          <div className="flex bg-amber-700 flex-1"></div>
+          <div className="flex bg-amber-800 flex-1"></div>
+          <div className="flex bg-blue-900 flex-1"></div>
+          <div className="flex bg-blue-800 flex-1"></div>
+          <div className="flex bg-blue-700 flex-1"></div>
+          <div className="flex bg-blue-600 flex-1"></div>
+          <div className="flex bg-blue-500 flex-1"></div>
         </div>
   </div>
 
